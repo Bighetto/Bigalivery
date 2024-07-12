3d0d1845e84def8b4b17b90201bcb351a47f71e8
Bighetto <arthurbighetto36@gmail.com>
2024-07-12 16:46:15 -0300

    modifier adicionado para adicionar estilo e definicoes na tela

--- app/src/main/java/com/example/bigalivery/MainActivity.kt
+++ app/src/main/java/com/example/bigalivery/MainActivity.kt
@@ -7,18 +7,22 @@ import android.provider.CalendarContract.Colors
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.bigalivery.ui.theme.BigaliveryTheme
 
 class MainActivity : ComponentActivity() {
@@ -71,6 +75,37 @@ private fun BoxPreview() {
     Box {
         Text(text = "Texto 1")
         Text(text = "Texto 2")
+    }
+    
+}
+
+@Preview
+@Composable
+private fun MyFirstCompositablePreview() {
+
+    Column(
+        Modifier
+            .padding(all = 26.dp)
+        .background(Color.Blue)
+            .height(300.dp)
+    ){
+        Text(text = "Texto 1")
+        Text(text = "Texto 2")
+        Row(Modifier.padding(horizontal = 26.dp)
+            .background(Color.Green)) {
+            Text(text = "Texto 3")
+            Text(text = "Texto 4")
+        }
+        Box (Modifier.background(Color.Red).padding(all = 22.dp).align(Alignment.CenterHorizontally)){
+            Row {
+                Text(text = "Texto 5")
+                Text(text = "Texto 6")
+            }
+            Column {
+                Text(text = "Texto 7")
+                Text(text = "Texto 8")
+            }
+        }
     }
     
 }
