ed8288d286062fca61c89b6a79b4fbeaed3376fb
Bighetto <arthurbighetto36@gmail.com>
2024-07-16 20:34:26 -0300

    adicionado estilizacao na borda do componente de produto

--- app/src/main/java/com/example/bigalivery/MainActivity.kt
+++ app/src/main/java/com/example/bigalivery/MainActivity.kt
@@ -16,11 +16,13 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -63,42 +65,45 @@ class MainActivity : ComponentActivity() {
     @Preview(showBackground = true)
     @Composable
     private fun ProductItem() {
+        Surface(modifier = Modifier.padding(10.dp), shape = RoundedCornerShape(15.dp), shadowElevation = 4.dp) {
+            Column(modifier = Modifier
+                .heightIn(250.dp, 300.dp)
+                .width(200.dp)) {
+                    val imageHeight = 100.dp
 
-        Column(modifier = Modifier
-            .height(250.dp)
-            .width(200.dp)) {
-            Box(modifier = Modifier
-                .height(100.dp)
-                .background(
-                    brush = Brush.horizontalGradient(
-                        colors = listOf(Purple200, Teal200)))
-                .fillMaxWidth()){
-                Image(painter = painterResource(
-                    id = R.drawable.ic_launcher_background),
-                    contentDescription = "Imagem do Produto",
-                    Modifier
-                        .size(100.dp)
-                        .offset(y = 50.dp)
-                        .clip(shape = CircleShape)
-                        .align(Alignment.BottomCenter)
-                )
-            }
-            Spacer(modifier = Modifier.height(50.dp))
-            Column(modifier = Modifier.padding(16.dp)) {
-                Text(text = LoremIpsum(50).values.first(),
-                    fontSize = 18.sp, //o sp permite que aumente ou dimiua o texto conforme usuario
-                    fontWeight = FontWeight(700),
-                    maxLines = 2,
-                    overflow = TextOverflow.Ellipsis
-                )
-                Text(text = "14,99",
-                    Modifier.padding(top = 8.dp),
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight(400),
-                )
-            }
-
+                    Box(modifier = Modifier
+                        .height(imageHeight)
+                        .background(
+                            brush = Brush.horizontalGradient(
+                                colors = listOf(Purple200, Teal200)
+                            )
+                        )
+                        .fillMaxWidth()){
+                        Image(painter = painterResource(
+                            id = R.drawable.ic_launcher_background),
+                            contentDescription = "Imagem do Produto",
+                            Modifier
+                                .size(imageHeight)
+                                .offset(y = imageHeight/2)
+                                .clip(shape = CircleShape)
+                                .align(Alignment.BottomCenter)
+                        )
+                    }
+                    Spacer(modifier = Modifier.height(imageHeight/2))
+                    Column(modifier = Modifier.padding(16.dp)) {
+                        Text(text = LoremIpsum(50).values.first(),
+                            fontSize = 18.sp, //o sp permite que aumente ou dimiua o texto conforme usuario
+                            fontWeight = FontWeight(700),
+                            maxLines = 2,
+                            overflow = TextOverflow.Ellipsis
+                        )
+                        Text(text = "14,99",
+                            Modifier.padding(top = 8.dp),
+                            fontSize = 14.sp,
+                            fontWeight = FontWeight(400),
+                        )
+                    }
+                }
         }
-        
     }
 }
